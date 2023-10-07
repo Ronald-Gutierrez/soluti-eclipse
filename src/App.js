b4f90e34b729aae61fa24856d5253e917b9aadda
Angel Choquehuanca Anconeyra <84652181+AngelAnconeyra@users.noreply.github.com>
2023-10-07 18:58:06 -0500

    Update App.js project

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+import { Questions } from "./components/Questions";
 import { Game } from "./components/Game";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
@@ -17,6 +18,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Questions />
       <Game />
       <Contact />
       <Footer />
