1e312948b3db0aba48db656ab3c572e2db14cc18
Erick Hualla <83515313+ehuallap@users.noreply.github.com>
2023-10-07 19:04:55 -0500

    Se añadió el componente al App

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Questions } from "./components/Questions";
 import { Game } from "./components/Game";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
+import Earth from "./components/Earth";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <Earth />
       <Skills />
       <Projects />
-      <Questions />
       <Game />
       <Contact />
       <Footer />
